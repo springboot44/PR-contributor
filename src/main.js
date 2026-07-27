f686347b1fe63f919528b000be50fd90d3206831
springboot44 <springlearning44@gmail.com>
2026-07-27 12:57:49 +0530

    Remove counter setup and button from main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import './style.css';
-import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>This is a website</h1>
-    <button id="counter"></button>
   </div>
 `;
-
-setupCounter(document.querySelector('#counter'));
