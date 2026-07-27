cf72b6f6eea00c768c026ae32d609d3f36a6b374
springboot44 <springlearning44@gmail.com>
2026-07-27 12:21:24 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './style.css';
-import { setupCounter } from './counter.js';
+
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1>This is a website</h1>
-    <button id="counter"></button>
+    <h1>This is a PR  website</h1>
+  
   </div>
 `;
 
-setupCounter(document.querySelector('#counter'));
+// setupCounter(document.querySelector('#counter'));
